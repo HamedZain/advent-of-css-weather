61b5b5ded361f3742234aa3c57b5145eeb595354
HamedZain <syrgst@gmail.com>
2022-03-28 02:10:44 +0300

    Use stored weather data

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,19 @@
 import { FC } from 'react'
-
+import './App.scss'
+import { DayCard } from "../components/DayCard/DayCard"
+import { weatherData } from '../data/weatherData'
 
 export const App: FC = () => {
 	return (
 		<div className="App">
-			<h1>Hello world</h1>
+
+			<div className="App__scrollable-container">
+				<div className="App__list">
+					{weatherData.map((day, index) => (
+						<DayCard key={`day-${index}`} {...day} />
+					))}
+				</div>
+			</div>
 
 		</div>
 	)
