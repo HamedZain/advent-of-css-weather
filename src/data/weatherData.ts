3e332bbbd2d699a11eb607ff063796ac43ae0d2a
HamedZain <syrgst@gmail.com>
2022-03-28 02:09:30 +0300

    Create weather data file

---
+++ src/data/weatherData.ts
@@ -0,0 +1,100 @@
+export const weatherData = [
+	{
+		date: '2019-01-01',
+		state: 'cloudy',
+		temperature: 71,
+		precipitation: 84,
+		low: 28
+	},
+	{
+		date: '2019-01-02',
+		state: 'sunny',
+		temperature: 65,
+		precipitation: 84,
+		low: 28
+	},
+	{
+		date: '2019-01-03',
+		state: 'stormy',
+		temperature: 67,
+		precipitation: 84,
+		low: 28
+	},
+	{
+		date: '2019-01-04',
+		state: 'snowy',
+		temperature: 32,
+		precipitation: 84,
+		low: 28
+	},
+	{
+		date: '2019-01-05',
+		state: 'partly-cloudy',
+		temperature: 57,
+		precipitation: 84,
+		low: 28
+	},
+	{
+		date: '2019-01-06',
+		state: 'rainy',
+		temperature: 63,
+		precipitation: 84,
+		low: 28
+	},
+	{
+		date: '2019-01-07',
+		state: 'sunny',
+		temperature: 71,
+		precipitation: 84,
+		low: 28
+	},
+	{
+		date: '2019-01-08',
+		state: 'cloudy',
+		temperature: 71,
+		precipitation: 84,
+		low: 28
+	},
+	{
+		date: '2019-01-09',
+		state: 'sunny',
+		temperature: 65,
+		precipitation: 84,
+		low: 28
+	},
+	{
+		date: '2019-01-10',
+		state: 'stormy',
+		temperature: 67,
+		precipitation: 84,
+		low: 28
+	},
+	{
+		date: '2019-01-11',
+		state: 'snowy',
+		temperature: 32,
+		precipitation: 84,
+		low: 28
+	},
+	{
+		date: '2019-01-12',
+		state: 'partly-cloudy',
+		temperature: 57,
+		precipitation: 84,
+		low: 28
+	},
+	{
+		date: '2019-01-13',
+		state: 'rainy',
+		temperature: 63,
+		precipitation: 84,
+		low: 28
+	},
+	{
+		date: '2019-01-14',
+		state: 'sunny',
+		temperature: 71,
+		precipitation: 84,
+		low: 28
+	}
+]
